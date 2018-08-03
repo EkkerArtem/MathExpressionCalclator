0b4fc95651de9b6df680328b13cebdfddea33130
artem.ekker <ekkerartem@gmail.com>
2018-08-03 16:40:23 +0300

    API declaration.

---
+++ src/main/java/StreamInterpreter.java
@@ -0,0 +1,3 @@
+public interface StreamInterpreter {
+    int interpret(String input);
+}
---
+++ src/main/java/StreamInterpreterImpl.java
@@ -0,0 +1,6 @@
+public class StreamInterpreterImpl implements StreamInterpreter {
+    @Override
+    public int interpret(String input) {
+        return 0;
+    }
+}
