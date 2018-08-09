f9b0b0a32d9ca8638db5c3f287bb143f608fcee4
artem.ekker <ekkerartem@gmail.com>
2018-08-09 12:27:35 +0300

    Switch case in BinaryFactory replaced with immutable map.

--- src/main/java/com/EkkerArtem/stream/calculator/finiteStateMachine/fsmimpl/BinaryFactory.java
+++ src/main/java/com/EkkerArtem/stream/calculator/finiteStateMachine/fsmimpl/BinaryFactory.java
@@ -5,32 +5,32 @@ import com.EkkerArtem.stream.calculator.operations.impl.Addition;
 import com.EkkerArtem.stream.calculator.operations.impl.Division;
 import com.EkkerArtem.stream.calculator.operations.impl.Multiplication;
 import com.EkkerArtem.stream.calculator.operations.impl.Subtraction;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 /**
  * Return needed binary operation.
  */
 public class BinaryFactory {
+
+    private static final Map<String, Operation> registry =
+            ImmutableMap.<String, Operation>builder()
+                    .put("+", new Addition())
+                    .put("-", new Subtraction())
+                    .put("*", new Multiplication())
+                    .put("/", new Division())
+                    .build();
+
     /**
      * @param value the char came from parser.
      * @return needed binary operation.
      */
     public Operation operationFactory(String value) {
-        switch (value) {
-            case "+": {
-                return new Addition();
-            }
-            case "-": {
-                return new Subtraction();
-            }
-            case "*": {
-                return new Multiplication();
-            }
-            case "/": {
-                return new Division();
-            }
-            default: {
-                throw new UnsupportedOperationException();
-            }
+        Operation operation = registry.get(value);
+        if (operation == null) {
+            throw new UnsupportedOperationException(value);
         }
+        return operation;
     }
 }
