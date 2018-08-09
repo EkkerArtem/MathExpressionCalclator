2e10b51c031ae0eff8509ec371000a2418455005
artem.ekker <ekkerartem@gmail.com>
2018-08-09 12:56:25 +0300

    Refactored AbstractStateMachine. Project has been divided into 2 modules. All switch cases replaced with immutable maps.

--- src/main/java/com/EkkerArtem/stream/calculator/finiteStateMachine/State.java
+++
@@ -1,8 +0,0 @@
-package com.EkkerArtem.stream.calculator.finiteStateMachine;
-
-/**
- * The enumeration of all states.
- */
-public enum State {
-    BINARY_OPERATION, INITIAL, NUMBER, OPEN_PARENTHESIS, CLOSE_PARENTHESIS
-}
